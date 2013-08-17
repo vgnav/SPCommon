50c5db4802c38588cf5f45261777ffd093eff276
Navid <navid.wali@gmail.com>
2013-08-17 10:33:17 +1000

    Added logger

--- SPCommon.Infrastructure/Common/Constants.cs
+++ SPCommon.Infrastructure/Common/Constants.cs
@@ -1,6 +1,14 @@
-﻿namespace SPCommon.Infrastructure.Common
+﻿using System.CodeDom;
+
+namespace SPCommon.Infrastructure.Common
 {
     public static class Constants
     {
+        public static class Logging
+        {
+            public const string AreaName = "Infastructure";
+            public const string ServiceName = "SPCommon Logging Service";
+
+        }
     }
 }
---
+++ SPCommon.Infrastructure/Common/SPLogger.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.SharePoint.Administration;
+using SPCommon.Interface;
+
+namespace SPCommon.Infrastructure.Common
+{
+    public class SPLogger : ILogger
+    {
+        #region Singleton 
+
+        private SPLogger() {}
+        private static SPLogger _instance;
+        public static SPLogger Instance
+        {
+            get { return _instance ?? (_instance = new SPLogger()); }
+        }
+
+        #endregion
+
+        public void Log(string message)
+        {
+            SPDiagnosticsService.Local.WriteTrace(
+                0,
+                new SPDiagnosticsCategory(Constants.Logging.AreaName, TraceSeverity.Medium, EventSeverity.Information),
+                TraceSeverity.Medium,
+                message,
+                null);
+        }
+
+        public void Log(Exception ex)
+        {
+            SPDiagnosticsService.Local.WriteTrace(
+                0, 
+                new SPDiagnosticsCategory(Constants.Logging.AreaName, TraceSeverity.Unexpected, EventSeverity.Error), 
+                TraceSeverity.Unexpected, 
+                ex.Message, 
+                ex.StackTrace);
+        }
+
+    }
+}
